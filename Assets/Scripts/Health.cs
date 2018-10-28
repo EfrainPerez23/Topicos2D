45211b7a022f02d56b8a3ec4ac3d8ac33165e938
Efrain Perez <efrain.abperez23@gmail.com>
2018-10-27 22:26:53 -0400

    added enemy damage and health

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+	public int health;
+	public int hearthNumbers;
+	public Image[] hearths;
+	public Sprite fullHearth;
+	public Sprite emptyHearth;
+
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+
+		if (this.health > this.hearthNumbers) {
+			this.health = this.hearthNumbers;
+		}
+		for(int i = 0; i < this.hearths.Length; i++) {
+
+			if (i < this.health) {
+				this.hearths[i].sprite = this.fullHearth;
+			} else {
+				this.hearths[i].sprite = this.emptyHearth;
+			}
+			
+
+			if (i <= this.hearthNumbers) {
+				this.hearths[i].enabled = true;
+			} else {
+				this.hearths[i].enabled = false;
+			}
+		}
+	}	
+	
+}
--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -14,24 +14,35 @@ public class Player_Controller : MonoBehaviour
     public float jumpPower = 6.5f;
     private bool jump;
 
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+    public Transform attackPos;
+    public float attackRange;
 
- //variable publica que permite acceder al animator desde este script
-	// Use this for initialization
-	void Start () 
-	{
+    public LayerMask whatIsEnemy;
+    public int damage;
+
+    private SpriteRenderer mySpriteRenderer;
+
+
+    //variable publica que permite acceder al animator desde este script
+    // Use this for initialization
+    void Start()
+    {
         rigiBody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
+        this.mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         //control de teclado
         velX = Input.GetAxisRaw("Horizontal");
         velY = rigiBody.velocity.y;
         rigiBody.velocity = new Vector2(velX * moveSpeed, velY);
 
-        if(velX!=0)
+        if (velX != 0)
         {
             caminar = true;
         }
@@ -41,20 +52,68 @@ public class Player_Controller : MonoBehaviour
         }
         anim.SetBool("caminar", caminar);
 
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             jump = true;
 
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            // flip the sprite
+            this.mySpriteRenderer.flipX = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            // flip the sprite
+            this.mySpriteRenderer.flipX = true;
+        }
+
+        if (this.timeBtwAttack <= 0) {
+            if (Input.GetKey(KeyCode.Space)) {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(this.attackPos.position, this.attackRange, this.whatIsEnemy);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(this.damage);
+                }
+            }
+            this.timeBtwAttack = this.startTimeBtwAttack;
+            
+        } else {
+            this.timeBtwAttack -= Time.deltaTime;
         }
     }
 
     void FixedUpdate()
     {
-        if(jump)
+        if (jump)
         {
             rigiBody.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             jump = false;
         }
 
+    }
+
+    /// <summary>
+    /// Callback to draw gizmos only if the object is selected.
+    /// </summary>
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.attackPos.position, this.attackRange);
+    }
+
+    /// <summary>
+    /// OnCollisionEnter is called when this collider/rigidbody has begun
+    /// touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Quitar vida");
+		if (other.gameObject.tag == GetComponent<Enemy>().tag) {
+			Debug.Log("Quitar vid 2a");
+		}
     }
 }
